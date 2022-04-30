c1ab32ab88948ad729a2571f7a0a58805f18a819
Miftahul Jannat Oysharja <98273718+OYSHARJA@users.noreply.github.com>
2022-04-30 13:25:33 +0600

    Final Task 05

---
+++ Lab Task 9/view/footer.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Home</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <style>
+        body{ font: 14px sans-serif; }
+        .wrapper{ width: 360px; padding: 20px; }
+        footer {
+  background-color: transparent;
+  margin-top: -0.5em;
+  padding: 2em;
+  position: fixed; 
+  left: 50%;
+  transform:translateX(-50%);
+  bottom: -3em;
+
+}
+    </style>
+</head>
+<body>
+
+<footer>
+  <div class="header-left">
+  <h5 style="text-align:left">© Copyright</h5>
+    <p> Wed-site Event Management 2022</p>
+  </div>
+</footer>
+
+
+</body>
+</html>
---
+++ Lab Task 9/view/home.php
@@ -0,0 +1,28 @@
+<?php
+include ("../view/header.php");
+?>
+<!DOCTYPE html>
+<html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Home</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <style>
+        body{ font: 14px sans-serif;
+        background-image: url('../assets/background.jpg'); }
+        .wrapper{ width: 360px; padding: 20px; }
+    </style>
+</head>
+    <body> 
+    <div style="padding-left:20px">
+    <h1>Welcome</h1><br>
+    <h2>to Wed-site</h2><br>
+    <h3>Event Management Page</h3><br>
+    </div>
+    </body>
+</html>
+
+<?php
+include ("footer.php");
+?>
